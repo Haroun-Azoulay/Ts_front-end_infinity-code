8ae9098e4747a63fb710eaa948cd603eb9da0b13
Haroun-Azoulay <ha.azoulay93@gmail.com>
2025-03-29 17:23:04 +0100

    [✅] Update components/AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,40 +1,68 @@
 <template>
   <header class="bg-white">
     <nav
-      class="mx-auto max-w-7xl items-center justify-between p-6 lg:px-8 lg:flex"
+      class="mx-auto max-w-7xl items-center flex justify-between p-6 lg:px-8"
       aria-label="Global"
     >
       <div class="flex">
-        <a href="#" class="-m-1.5 p-1.5">
-          <span class="sr-only">Your Company</span>
+        <a href="#" class="m-1.5 p-1.5">
           <!-- Logo about my company. -->
           <img
-            class="mt-4 w-96 size-40"
+class="w-30 h-20 sm:w-40 sm:h-24 md:w-52 md:h-32 lg:w-60 lg:h-36 xl:w-64 xl:h-40 2xl:w-72 2xl:h-48 mx-auto mt-4"
             src="../assets/logo_infinityCode.png"
-            alt=""
+            alt="Logo Infinity Code"
           />
         </a>
       </div>
+      <div class="flex lg:hidden ml-auto items-center">
+        <button type="button" class="inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = true">
+          <span class="sr-only">Open main menu</span>
+          <Bars3Icon class="size-10 md:size-12 text-blue-900" aria-hidden="true" />
+        </button>
+      </div>
       <PopoverGroup class="hidden lg:flex lg:gap-x-12 items-center justify-center">
         <div class="lg:flex lg:gap-x-12 items-center">
-        <a href="#" class="font-semibold text-gray-900 hover:text-blue-900 text-xl">Accueil</A>
-        <a href="#" class="font-semibold text-gray-900 hover:text-blue-900 text-xl">Qui suis-je ?</a>
-        <a href="#" class="font-semibold text-gray-900 hover:text-blue-900 text-xl">Contact</a>
+          <a href="#" class="font-semibold text-gray-900 hover:text-blue-900 text-xl">Accueil</A>
+          <a href="#" class="font-semibold text-gray-900 hover:text-blue-900 text-xl">Qui suis-je ?</a>
+          <a href="#" class="font-semibold text-gray-900 hover:text-blue-900 text-xl">Contact</a>
+        </div>
+      </PopoverGroup>
+      <div class="flex">
+        <button class="hidden lg:inline-block bg-gradient-to-bl from-blue-700 to-blue-900 hover:scale-105 transition-transform text-white font-bold px-7 py-2 rounded-full">
+          Rendez-vous gratuit
+        </button>
+      </div>
+    </nav>
+    <Dialog class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
+      <div class="fixed inset-0 z-10" />
+      <DialogPanel class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <div class="flex items-center justify-between">
+          <a href="#" class="-m-1.5 p-1.5">
+            <img class="w-30 h-20" src="../assets/logo_infinityCode.png" alt="Logo Infinity Code">
+          </a>
+          <button type="button" class="m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
+            <span class="sr-only">Close menu</span>
+            <XMarkIcon class="size-12 text-blue-900" aria-hidden="true" />
+          </button>
+        </div>
+        <div class="mt-6 flow-root">
+          <div class="-my-6 divide-y divide-gray-500/10">
+            <div class="space-y-2 py-6">
+              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold  hover:text-blue-900">Accueil</a>
+              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold  hover:text-blue-900">Qui suis-je ?</a>
+              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold  hover:text-blue-900">Contact</a>
+              <button class="mt-3 bg-gradient-to-bl from-blue-700 to-blue-900 hover:scale-105 transition-transform text-white font-bold px-7 py-2 rounded-full">
+          Rendez-vous gratuit
+        </button>
+            </div>
           </div>
-             </PopoverGroup>
-       <div class="flex">
-             <button class="bg-linear-to-bl from-blue-700 to-blue-900 hover:scale-105 text-white font-bold px-7 py-2 rounded-full">
-  Rendez-vous gratuit
-</button>
-
-          </div>
-
-         </nav>
-
+        </div>
+      </DialogPanel>
+    </Dialog>
   </header>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { ref } from 'vue'
 import {
   Dialog,
@@ -56,44 +84,5 @@ import {
   SquaresPlusIcon,
   XMarkIcon,
 } from '@heroicons/vue/24/outline'
-import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/vue/20/solid'
-
-const products = [
-  {
-    name: 'Analytics',
-    description: 'Get a better understanding of your traffic',
-    href: '#',
-    icon: ChartPieIcon,
-  },
-  {
-    name: 'Engagement',
-    description: 'Speak directly to your customers',
-    href: '#',
-    icon: CursorArrowRaysIcon,
-  },
-  {
-    name: 'Security',
-    description: 'Your customers’ data will be safe and secure',
-    href: '#',
-    icon: FingerPrintIcon,
-  },
-  {
-    name: 'Integrations',
-    description: 'Connect with third-party tools',
-    href: '#',
-    icon: SquaresPlusIcon,
-  },
-  {
-    name: 'Automations',
-    description: 'Build strategic funnels that will convert',
-    href: '#',
-    icon: ArrowPathIcon,
-  },
-]
-const callsToAction = [
-  { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
-  { name: 'Contact sales', href: '#', icon: PhoneIcon },
-]
-
 const mobileMenuOpen = ref(false)
 </script>
